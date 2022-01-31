d7644012e8470d11f04f1a775f45b5ba8df15de8
TheSalt <rishab.chakrabarti@gmail.com>
2022-01-31 21:49:59 +0530

    FINALE

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from keras.layers import Dense, LSTM
 
 # get data from yahoo of any company
 data_frame = web.DataReader(
-    'AMD', data_source='yahoo', start='2010-01-01', end='2021-12-31')
+    'AMZN', data_source='yahoo', start='2010-01-01', end='2022-01-31')
 # see the data
 data_frame
 #rows and columns
@@ -31,10 +31,10 @@ scaled_data
 training_data = scaled_data[0:training_data_len, :]
 x_train = []
 y_train = []
-for i in range(100, len(training_data)):
-    x_train.append(training_data[i-100:i, 0])
+for i in range(60, len(training_data)):
+    x_train.append(training_data[i-60:i, 0])
     y_train.append(training_data[i, 0])
-    if i <= 100:
+    if i <= 60:
         print(x_train)
         print(y_train)
         print()
@@ -53,11 +53,11 @@ model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, batch_size=1, epochs=1)
 # creating test-data set
-test_data = scaled_data[training_data_len-100:, :]
+test_data = scaled_data[training_data_len-60:, :]
 x_test = []
 y_test = data_close_set[training_data_len:, :]
-for i in range(100, len(test_data)):
-    x_test.append(test_data[i-100:i, 0])
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
 # make data into numpy
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
@@ -85,11 +85,11 @@ plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
 plt.show()
 
 valid
-# Analysis on last 100 days to predict the price of the stock
+# Analysis on last 60 days to predict the price of the stock
 stock_data = web.DataReader(
-    'AMD', data_source='yahoo', start='2010-01-01', end='2021-12-31')
+    'AMZN', data_source='yahoo', start='2021-01-01', end='2022-01-31')
 stock_data_close = stock_data.filter(['Close'])
-stock_data_close_set = stock_data_close[-100:].values
+stock_data_close_set = stock_data_close[-60:].values
 days_scaled = scale.transform(stock_data_close_set)
 X_test_sample = []
 X_test_sample.append(days_scaled)
@@ -101,10 +101,12 @@ predicted_price = scale.inverse_transform(predicted_price)
 print(predicted_price)
 
 stock_data_actual = web.DataReader(
-    'AMD', data_source='yahoo', start='2022-01-01', end='2022-01-01')
+    'AMZN', data_source='yahoo', start='2022-01-31', end='2022-01-31')
 print(stock_data_actual['Close'])
 
 
 # accuracy
 print("Accuracy of the model is:")
-print(predicted_price[0][0]/stock_data_actual['Close'][0]*100)
+error_percent = abs(
+    (predicted_price[0][0]-stock_data_actual['Close'][0])/stock_data_actual['Close'][0])*100
+print(100-error_percent)
